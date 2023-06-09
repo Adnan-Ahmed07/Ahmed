2500716f3a77a2394d0ff6ed51a629f40da68908
Adnan52443784 <96421546+Adnan52443784@users.noreply.github.com>
2023-06-09 03:31:04 -0700

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{  
+    int i=0;
+    int marks[4]={14,25,45,65};
+    while (i<4)
+    {
+        cout<<"The marks: "<<marks[i]<<endl;
+        
+    }
+    
+ return 0;
+}
